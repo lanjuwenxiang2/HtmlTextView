9287af077a26dd3ede5bb3095f9ab6ddd1e75421
ljwx <1146699396@qq.com>
2020-04-07 15:39:12 +0800

    新增设置文字颜色,大小,是否加粗,优化部分逻辑

--- app/src/main/java/com/ljwx/view/MainActivity.java
+++ app/src/main/java/com/ljwx/view/MainActivity.java
@@ -3,6 +3,7 @@ package com.ljwx.view;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,10 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final HtmlTextView test = findViewById(R.id.test);
         ClickAreaImageView view = findViewById(R.id.img);
         view.setOnClickListenerTag1(new ClickAreaImageView.TouchAreaListener() {
             @Override
             public void call() {
+//                test.setCenterString("测试文本内容");
+//                test.setColorRight(getResources().getColor(R.color.colorPrimary));
+//                test.setBoldRight(true);
+//                test.setSizeCenter(90);
                 Toast.makeText(MainActivity.this, "tag1", Toast.LENGTH_SHORT).show();
             }
         });
--- htmltextview/src/main/java/com/ljwx/view/HtmlTextView.java
+++ htmltextview/src/main/java/com/ljwx/view/HtmlTextView.java
@@ -13,33 +13,48 @@ import android.widget.TextView;
 import com.ljwx.htmltextview.R;
 
 public final class HtmlTextView extends TextView {
+
+    //默认颜色,大小
     private int defualtColor = getCurrentTextColor();
     private float defualtSize = getTextSize();
     private int mNorColor;
     private float mNorSize;
 
     private String mLeftString = "", mCenterString = "", mRightString = "";
+    //文字颜色
     private int mColorLeft = defualtColor, mColorCenter = defualtColor, mColorRight = defualtColor;
+    //文字大小
     private float mSizeLeft, mSizeCenter, mSizeRight;
-    //底部margin
+    //调整文字距底部margin
     private float mMarBtmLeft, mMarBtmCenter, mMarBtmRight;
+    //字体是否加粗
     private boolean mBoldLeft, mBoldCenter, mBoldRight;
     //中间文本的左右margin
     private float mCenterTvMarginLeft, mCenterTvMarginRight;
-    //模式
+    //字体分布模式(从左往右排列模式,整行平分模式)
     private int mGravityMode;
+    //字体对齐(底部对齐,中间线对齐)
     private int mBaseLineMode;
+    //绘制文字的画笔
     private Paint mPaintLeft = new Paint(Paint.ANTI_ALIAS_FLAG);
     private Paint mPaintCenter = new Paint(Paint.ANTI_ALIAS_FLAG);
     private Paint mPaintRight = new Paint(Paint.ANTI_ALIAS_FLAG);
+    //最大文字的画笔
     private Paint mMaxPaint;
+    //最高文字基线
     private Float mMaxBaseLine;
     //drawable宽高
     private float mDrawableLeftWidth, mDrawableRightWidth, mDrawableLeftHeight, mDrawableRightHeight;
+    //当有drawable图片时,距离文字的间距
     private float mDrawableLeftPadding, mDrawableRightPadding;
+    //drawable图片数组
     private Drawable[] drawables;
+    //文字的宽度
     private float mLeftWidth, mCenterWidth, mRightWidth;
-    private int mParentWidth, mViewWidth, mContentWidth;
+    //父布局宽度,控件宽度
+    private int mParentWidth, mViewWidth;
+    //文本所需总宽度
+    private int mContentWidth;
     //字体大小自适应
     private boolean mAutoSize = false;
     private float mAutoSizeRatio = 0.01f;
@@ -93,6 +108,9 @@ public final class HtmlTextView extends TextView {
         super(context, attrs, defStyleAttr);
     }
 
+    /**
+     * 初始化默认画笔、drawable
+     */
     private void init() {
         mPaintLeft.setFakeBoldText(mBoldLeft);
         mPaintLeft.setTextSize(mSizeLeft);
@@ -108,11 +126,13 @@ public final class HtmlTextView extends TextView {
         mPaintRight.setTextSize(mSizeRight);
         mPaintRight.setColor(mColorRight);
         mPaintRight.setTypeface(getTypeface());
-        initMaxPaint();
-        initDrawable();
+        drawableSize();
     }
 
-    private void initDrawable() {
+    /**
+     * 当设置了drawable,没有手动设置宽高时,使用默认宽高
+     */
+    private void drawableSize() {
         drawables = getCompoundDrawables();
         setCompoundDrawables(null, drawables[1], null, drawables[3]);
         if (drawables[0] != null) {
@@ -126,9 +146,9 @@ public final class HtmlTextView extends TextView {
     }
 
     /**
-     * 计算最大高度
+     * 获取最大文字高度
      */
-    private void initMaxPaint() {
+    private void getMaxPaint() {
         mMaxPaint = mPaintLeft;
         mMaxPaint = mPaintCenter.getTextSize() >= mPaintLeft.getTextSize() ? mPaintCenter : mPaintLeft;
         mMaxPaint = mPaintRight.getTextSize() >= mMaxPaint.getTextSize() ? mPaintRight : mMaxPaint;
@@ -146,9 +166,13 @@ public final class HtmlTextView extends TextView {
 
         int height = MeasureSpec.getSize(heightMeasureSpec);
         int width = MeasureSpec.getSize(widthMeasureSpec);
-
+        //最大高度Paint
+        getMaxPaint();
+        //原生控件文本内容
         String txt = getText().toString();
+        //获取最大高度(1,没有尺寸限制,上限为父view 2,没有尺寸限制,没有上限)
         if (MeasureSpec.AT_MOST == heightMode || MeasureSpec.UNSPECIFIED == heightMode) {
+            //高度上的padding值
             float ptm = (float) (getPaddingTop() + getPaddingBottom() + 0.5);
             if (!TextUtils.isEmpty(txt)) {
 //                height = (int) (getPaint().descent() - getPaint().ascent() + ptm);
@@ -157,37 +181,44 @@ public final class HtmlTextView extends TextView {
                 height = (int) (mMaxPaint.descent() - mMaxPaint.ascent() + ptm);
             }
         }
-
+        //获取最大宽度(没有尺寸限制,上限为父view)
         if (MeasureSpec.AT_MOST == widthMode) {
             if (!TextUtils.isEmpty(txt)) {
 //                width = (int) (getPaint().measureText(getText().toString()) + plr);
                 width = getMeasuredWidth();
             } else {
-                width = contentWidth();
+                //drawablePadding
+                int drawablePadding = (int) (mDrawableLeftPadding + mDrawableRightPadding + 0.5);
+                width = contentWidth()+drawablePadding;
             }
         }
+        //获取最大高度(文字高度,图片高度比较)
         height = (int) Math.max(height, Math.max(mDrawableLeftHeight, mDrawableRightHeight));
         setMeasuredDimension(width, height);
         contentWidth();
     }
 
     /**
-     * 计算文本宽度
+     * 计算文本所需总宽度
      */
     private int contentWidth() {
+        //左右两边padding值
         float plr = (float) (getPaddingLeft() + getPaddingRight() + 0.5);
-        float w1 = 0, w2 = 0, w3 = 0;
+        //单个文本宽度
+        float left = 0, center = 0, right = 0;
         if (!TextUtils.isEmpty(mLeftString)) {
-            w1 = mPaintLeft.measureText(mLeftString);
+            left = mPaintLeft.measureText(mLeftString);
         }
         if (!TextUtils.isEmpty(mCenterString)) {
-            w2 = mPaintCenter.measureText(mCenterString);
+            center = mPaintCenter.measureText(mCenterString);
         }
         if (!TextUtils.isEmpty(mRightString)) {
-            w3 = mPaintRight.measureText(mRightString);
+            right = mPaintRight.measureText(mRightString);
         }
-        mContentWidth = (int) (w1 + w2 + w3 + plr
+        mContentWidth = (int) (left + center + right + plr
+                //margin值
                 + mCenterTvMarginLeft + mCenterTvMarginRight
+                //drawable所占宽度
                 + mDrawableLeftWidth + mDrawableRightWidth);
         return mContentWidth;
     }
@@ -268,11 +299,14 @@ public final class HtmlTextView extends TextView {
     private final void drawCenter(Canvas canvas) {
         if (!TextUtils.isEmpty(mCenterString)) {
             float xStartPoint = mCenterTvMarginLeft + mLeftWidth;
-            //填满
+            //平分模式时
             if (mGravityMode != 0) {
-                int midx = (getWidth() - getPaddingLeft() - getPaddingRight()) / 2;
-                xStartPoint = midx - mPaintCenter.measureText(mCenterString) / 2;
-                //填满没有right时
+                //x方向的内容中间值
+                int padding = getPaddingLeft() + getPaddingRight();
+                int midX = (getWidth() - 0) / 2;
+                //获得中间文本的x起始值
+                xStartPoint = midX - mPaintCenter.measureText(mCenterString) / 2;
+                //平分模式,右边文本为空
                 if (TextUtils.isEmpty(mRightString)) {
                     xStartPoint = getWidth() - getPaddingRight() - mPaintCenter.measureText(mCenterString)
                             - mDrawableRightWidth - mDrawableRightPadding;
@@ -286,7 +320,7 @@ public final class HtmlTextView extends TextView {
     private final void drawRight(Canvas canvas) {
         if (!TextUtils.isEmpty(mRightString)) {
             float xStartPoint = mLeftWidth + mCenterWidth;
-            //填满
+            //平分模式
             if (mGravityMode != 0) {
                 xStartPoint = (float) (this.getWidth() - this.getPaddingRight()) - mPaintRight.measureText(mRightString) - mDrawableRightWidth - mDrawableRightPadding;
             }
@@ -296,7 +330,7 @@ public final class HtmlTextView extends TextView {
     }
 
     /**
-     * 基线对齐
+     * 计算基线对齐
      */
     private final float countBaseLine(Paint paint) {
         float tvLine = (getHeight() / 2) + Math.abs(paint.ascent() + paint.descent()) / 2;
@@ -329,9 +363,15 @@ public final class HtmlTextView extends TextView {
             //固定宽高
             if (mDrawableRightWidth > 0 && mDrawableRightHeight > 0) {
                 float rightStartPoint = mLeftWidth + mCenterWidth + mRightWidth + mDrawableRightPadding;
-                //空间是否足够,紧凑模式
-                int leftPoint = (int) ((getWidth() >= rightStartPoint + mDrawableRightWidth && mGravityMode == 0) ? (rightStartPoint) : (getWidth() - mDrawableRightWidth));
                 int topPoint = (int) (getHeight() / 2 - mDrawableRightHeight / 2);
+                int leftPoint;
+                //顺序排列模式
+                if (mGravityMode == 0) {
+                    leftPoint = (int) ((getWidth() >= rightStartPoint + mDrawableRightWidth) ? (rightStartPoint) : (getWidth() - mDrawableRightWidth));
+                    paddingRight = 0;
+                } else {
+                    leftPoint = (int) (getWidth() - mDrawableRightWidth);
+                }
                 right.setBounds(leftPoint - paddingRight, topPoint, (int) mDrawableRightWidth + leftPoint - paddingRight, (int) mDrawableRightHeight + topPoint);
             } else {
                 //原生宽高
@@ -342,40 +382,50 @@ public final class HtmlTextView extends TextView {
         }
     }
 
+    /**
+     * 设置、获取文字
+     *
+     * @param: mXXXString 文字
+     */
     public String getLeftString() {
         return mLeftString;
     }
 
-    public void setLeftString(String mTv1String) {
-        this.mLeftString = mTv1String;
-        requestLayout();
+    public void setLeftString(String mLeftString) {
+        this.mLeftString = mLeftString;
+        invalidate();
     }
 
     public String getCenterString() {
         return mCenterString;
     }
 
-    public void setCenterString(String mTv2String) {
-        this.mCenterString = mTv2String;
-        requestLayout();
+    public void setCenterString(String mCenterString) {
+        this.mCenterString = mCenterString;
+        invalidate();
     }
 
     public String getRightString() {
         return mRightString;
     }
 
-    public void setRightString(String mTv3String) {
-        this.mRightString = mTv3String;
-        requestLayout();
+    public void setRightString(String mRightString) {
+        this.mRightString = mRightString;
+        invalidate();
     }
 
+    /**
+     * 获取、设置文字大小
+     *
+     * @param: mSizeXXX  文字大小
+     */
     public float getSizeLeft() {
         return mSizeLeft;
     }
 
-    public void setSizeLeft(float mTv1Size) {
-        mPaintLeft.setTextSize(mTv1Size);
-        requestLayout();
+    public void setSizeLeft(float mLeftSize) {
+        mPaintLeft.setTextSize(mLeftSize);
+        invalidate();
     }
 
     public float getSizeCenter() {
@@ -384,7 +434,7 @@ public final class HtmlTextView extends TextView {
 
     public void setSizeCenter(float centerSize) {
         mPaintCenter.setTextSize(centerSize);
-        requestLayout();
+        invalidate();
     }
 
     public float getSizeRight() {
@@ -393,6 +443,75 @@ public final class HtmlTextView extends TextView {
 
     public void setSizeRight(float rightSize) {
         mPaintRight.setTextSize(rightSize);
-        requestLayout();
+        invalidate();
+    }
+
+    /**
+     * 获取、设置文字颜色
+     */
+    public int getColorLeft() {
+        if (mPaintLeft != null) {
+            return mPaintLeft.getColor();
+        }
+        return 0;
+    }
+
+    public void setColorLeft(int color) {
+        mPaintLeft.setColor(color);
+        invalidate();
+    }
+
+    public int getColorCenter() {
+        if (mPaintCenter != null) {
+            return mPaintCenter.getColor();
+        }
+        return 0;
+    }
+
+    public void setColorCenter(int color) {
+        mPaintCenter.setColor(color);
+        invalidate();
+    }
+
+    public int getColorRight() {
+        if (mPaintRight != null) {
+            return mPaintRight.getColor();
+        }
+        return 0;
+    }
+
+    public void setColorRight(int color) {
+        mPaintRight.setColor(color);
+        invalidate();
+    }
+
+    /**
+     * 获取、设置文字是否加粗
+     */
+    public void setBoldLeft(boolean bold) {
+        mPaintLeft.setFakeBoldText(bold);
+        invalidate();
+    }
+
+    public void getBoldLeft() {
+        mPaintLeft.isFakeBoldText();
+    }
+
+    public void setBoldCenter(boolean bold) {
+        mPaintCenter.setFakeBoldText(bold);
+        invalidate();
+    }
+
+    public void getBoldCenter() {
+        mPaintCenter.isFakeBoldText();
+    }
+
+    public void setBoldRight(boolean bold) {
+        mPaintRight.setFakeBoldText(bold);
+        invalidate();
+    }
+
+    public void getBoldRight() {
+        mPaintRight.isFakeBoldText();
     }
 }
